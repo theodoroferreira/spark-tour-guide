f8c936d053125c74b65be0ce3f9c6aa72d9f569e
MaiiZuko <maiaramzucco@gmail.com>
2025-06-12 20:08:57 -0300

    caminhos
    caminho pra parque da pedreira arrumado

--- scripts/Locations/ParqueDaPedreira.gd
+++ scripts/Locations/ParqueDaPedreira.gd
@@ -6,7 +6,17 @@ func _ready():
 	else:
 		print("ERRO: HomeButton não encontrado!")
 
+	if has_node("DeckButton"):
+		$DeckButton.pressed.connect(_on_deck_button_pressed)
+	else:
+		print("ERRO: DeckButton não encontrado!")
+
 func _on_home_button_pressed():
 	print("Botão Home clicado!")
 	get_tree().call_deferred("change_scene_to_file", "res://scenes/Home.tscn")
-	GameManager.current_location = "Home" 
+	GameManager.current_location = "Home"
+
+func _on_deck_button_pressed():
+	print("Botão Deck clicado!")
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/Locations/DeckGame.tscn")
+	GameManager.current_location = "DeckGame" 

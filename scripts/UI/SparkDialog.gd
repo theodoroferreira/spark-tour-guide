967349444554cf8849ca0f1b247c8c7f70e67750
Theodoro Gaspar Ferreira <theogferreira@outlook.com>
2025-06-25 11:50:13 -0300

    dialog sound

--- scripts/UI/SparkDialog.gd
+++ scripts/UI/SparkDialog.gd
@@ -5,12 +5,14 @@ signal messages_finished
 @onready var dialog_text = $DialogText
 @onready var next_button = $NextButton
 @onready var spark_icon = $SparkIcon
+@onready var dialog_sound = $DialogSound
 
 var message_queue = []
 
 func _ready():
 	next_button.pressed.connect(_on_next_button_pressed)
 	load_spark_icon()
+	_setup_dialog_sound()
 	hide()
 
 func load_spark_icon():
@@ -33,6 +35,22 @@ func display_next_message():
 		return
 
 	dialog_text.text = message_queue.pop_front()
+	_play_dialog_sound()
 
 func _on_next_button_pressed():
 	display_next_message()
+
+func _setup_dialog_sound():
+	if not has_node("DialogSound"):
+		var audio_player = AudioStreamPlayer.new()
+		audio_player.name = "DialogSound"
+		add_child(audio_player)
+		dialog_sound = audio_player
+	
+	var sound_path = "res://assets/sounds/sfx/dialog_sound.wav"
+	if ResourceLoader.exists(sound_path):
+		dialog_sound.stream = load(sound_path)
+
+func _play_dialog_sound():
+	if dialog_sound and dialog_sound.stream:
+		dialog_sound.play()
